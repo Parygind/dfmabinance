48f67978a3c865d8aa5582a960e3ee1e0915c2c2
parygind <parygind@gmail.com>
2020-07-13 16:20:57 +0600

    stop loss take profit+++

--- Bot.py
+++ Bot.py
@@ -37,7 +37,7 @@ def start(update, context):
     update.message.reply_text('Hi! Use /set <seconds> to set a timer')
 
 def count(update, context):
-    update.message.reply_text('Кол-во пар : ' + str(len(symb_list)) + ', take profit : ' + str(tk) + ', stop loss' + str(sl))
+    update.message.reply_text('Кол-во пар : ' + str(len(symb_list)) + ', take profit : ' + str(tk) + ', stop loss : ' + str(sl))
 
 def updateData(context):
     global dict_prev, dict_curr, symb_list
@@ -45,7 +45,7 @@ def updateData(context):
     dict_curr = dict()
 
     for pr in bin_bot.ticker24hr():
-        if pr['symbol'][-3:] == 'BTC' and float(pr['quoteVolume']) >= 0.00001:
+        if pr['symbol'][-3:] == 'BTC' and float(pr['quoteVolume']) >= 0.00001 and float(pr['lastPrice']) >= 0.0001:
             dict_curr[pr['symbol']] = float(pr['quoteVolume'])
 
     symb_list = list(dict_curr.keys())
@@ -64,11 +64,11 @@ def alarm1(context):
         course = float(inf[0][4])
 
         if symb_list[i] in dict_order:
-            if course > dict_order[symb_list[i]] * 1.1:
+            if course - dict_order[symb_list[i]] >= 0.000003:
                 tk = tk + 1
                 mesOrd = mesOrd + 'Профит ' + symb_list[i] + ' ' + float_to_str(course) + ' ' + dict_order[symb_list[i]] + ' '
                 del dict_order[symb_list[i]]
-            elif course < dict_order[symb_list[i]] * 0.9:
+            elif course - dict_order[symb_list[i]] <= -0.000003:
                 sl = sl + 1
                 mesOrd = mesOrd + 'Убыток ' + symb_list[i] + ' ' + float_to_str(course) + ' ' + dict_order[symb_list[i]] + ' '
                 del dict_order[symb_list[i]]
