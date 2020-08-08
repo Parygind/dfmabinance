fa1f2b8ec5ee0bc7d66ab68ff827646e2a83a73e
parygind <parygind@gmail.com>
2020-08-08 10:26:29 +0600

    верну алгоритм по объемам

--- Bot.py
+++ Bot.py
@@ -205,7 +205,8 @@ def alarm2(context):
         if vol >= dict_curr[symb_list[i]]*0.02:
             if not symb_list[i] in dict_order:
                 dict_order[symb_list[i]] = course
-            mesVol += symb_list[i] + '(+' + str(round(vol, 2)) + ' / ' + str(round((vol/dict_curr[symb_list[i]])*100, 2)) + '%) '
+            mesVol += symb_list[i] + '(+' + str(round(vol, 2)) + ' / ' + str(round((vol/dict_curr[symb_list[i]])*100, 2)) + '%)\n'
+            mesVol += str(inf) + '\n'
     c += 1
 
     if len(mesVol) > 0:
