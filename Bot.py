5572bfe4d57e8b5ea6a50b4bb26a2e1358d2828b
parygind <parygind@gmail.com>
2021-04-07 08:30:34 +0600

    fix

--- Bot.py
+++ Bot.py
@@ -60,7 +60,7 @@ dict_prev_min = dict()
 
 dict_book = dict()
 
-order_price = 500
+order_price = 700
 
 trade_on = True
 
@@ -126,7 +126,7 @@ def updateData(context):
                 and tickers[pr]['symbol'] != 'SUSD/USDT' and tickers[pr]['symbol'] != 'LINK/BTC' and tickers[pr]['symbol'] != 'DCR/BTC'\
                 and tickers[pr]['symbol'] != 'ENG/BTC' and tickers[pr]['symbol'] != 'LEND/BTC' and tickers[pr]['symbol'] != 'LUN/BTC' and tickers[pr]['symbol'] != 'PAX/USDT'\
                 and tickers[pr]['symbol'].find('DOWN') == -1 and tickers[pr]['symbol'].find('UP') == -1 \
-                and tickers[pr]['symbol'] != 'TUSD/BTC' and tickers[pr]['symbol'] != 'TUSD/USDT' and tickers[pr]['symbol'] != 'PAX/BTC' and tickers[pr]['symbol'] != 'DAI/BTC' and tickers[pr]['symbol'] != 'SUN/USDT':
+                and tickers[pr]['symbol'] != 'ASR/USDT' and tickers[pr]['symbol'] != 'TUSD/BTC' and tickers[pr]['symbol'] != 'TUSD/USDT' and tickers[pr]['symbol'] != 'PAX/BTC' and tickers[pr]['symbol'] != 'DAI/BTC' and tickers[pr]['symbol'] != 'SUN/USDT':
             dict_curr[tickers[pr]['symbol']] = float(tickers[pr]['quoteVolume'])
             market = bin_bot.market(tickers[pr]['symbol'])
             dict_prec[tickers[pr]['symbol']] = int(market['precision']['price'])
