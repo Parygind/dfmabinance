8012a0df0e1ed47edbab5768a06866f8b882ad39
parygind <parygind@gmail.com>
2021-07-05 18:52:32 +0600

    меньшие объемы

--- Bot.py
+++ Bot.py
@@ -660,7 +660,7 @@ def print_stream_data_from_stream_buffer(binance_websocket_api_manager):
                                 prevVol += e[1]
                             elif (t - e[0]) / 1000 <= 45:
                                 if step == 2:
-                                    if prevVol >= dict_curr[symb] * (0.021 * (30/60)) and price / dict_list[symb][0][2] > 1.005:
+                                    if prevVol >= dict_curr[symb] * (0.015 * (30/60)) and price / dict_list[symb][0][2] > 1.005:
                                         inf = get_klines1(symb, '1m', int((time.time() - 300) * 1000), 5)
                                         min_price = 999
                                         max_price = 0
@@ -743,7 +743,7 @@ def print_stream_data_from_stream_buffer(binance_websocket_api_manager):
                                 prevVol += e[1]
                             elif (t - e[0]) / 1000 <= 60:
                                 if step == 3:
-                                    if prevVol >= dict_curr[symb] * (0.021 * (45/60))  and price / dict_list[symb][0][2] > 1.005:
+                                    if prevVol >= dict_curr[symb] * (0.015 * (45/60))  and price / dict_list[symb][0][2] > 1.005:
                                         inf = get_klines1(symb, '1m', int((time.time() - 300) * 1000), 5)
                                         min_price = 999
                                         max_price = 0
@@ -823,7 +823,7 @@ def print_stream_data_from_stream_buffer(binance_websocket_api_manager):
                                 step = 4
                                 prevVol += e[1]
 
-                                if prevVol >= dict_curr[symb] * 0.021 and price / dict_list[symb][0][2] > 1.005:
+                                if prevVol >= dict_curr[symb] * 0.015 and price / dict_list[symb][0][2] > 1.005:
                                     inf = get_klines1(symb, '1m', int((time.time() - 300) * 1000), 5)
                                     min_price = 999
                                     max_price = 0
