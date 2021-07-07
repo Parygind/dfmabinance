a38a3a4bb9b5acf4ac7e92e8653237627d4be1f3
parygind <parygind@gmail.com>
2021-07-07 21:18:08 +0600

    Новый алгоритм

--- Bot.py
+++ Bot.py
@@ -13,6 +13,17 @@ import unicorn_binance_websocket_api
 import math
 import threading
 from datetime import datetime
+import queue
+
+#somewhere accessible to both:
+callback_queue = queue.Queue()
+
+def from_dummy_thread(func_to_call_from_main_thread):
+    callback_queue.put(func_to_call_from_main_thread)
+
+def from_main_thread_blocking():
+    callback = callback_queue.get() #blocks until an item is available
+    callback()
 
 # create a new context for this task
 ctx = decimal.Context()
@@ -624,4 +635,6 @@ for channel in channels:
             i += 1
 updater.bot.send_message(chat_id='-1001242337520', text='Запуск!')
 
-updater.idle()
+#updater.idle()
+while True:
+    from_main_thread_blocking()
