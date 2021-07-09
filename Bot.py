a3da376ff39833284aeb175a6faa93a6f5facad4
Dmitriy Parygin <parygind@gmail.com>
2021-07-09 16:59:29 +0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -426,7 +426,7 @@ def print_stream_data_from_stream_buffer(binance_websocket_api_manager):
                                     prevVol += e[1]
                                 elif (t - e[0]) / 1000 <= 45:
                                     if step == 2:
-                                        if prevVol >= dict_curr[symb] * (0.021 * (30/60)) and price / e[2] > 1.004:
+                                        if prevVol >= dict_curr[symb] * (0.017 * (30/60)) and price / e[2] > 1.003:
                                             inf = get_klines1(symb_USDT, '1m', None, 5)
                                             price = float(inf[4][4])
                                             min_price = 999
@@ -492,7 +492,7 @@ def print_stream_data_from_stream_buffer(binance_websocket_api_manager):
                                     prevVol += e[1]
                                 elif (t - e[0]) / 1000 <= 60:
                                     if step == 3:
-                                        if prevVol >= dict_curr[symb] * (0.021 * (45/60)) and price / e[2] > 1.005:
+                                        if prevVol >= dict_curr[symb] * (0.017 * (45/60)) and price / e[2] > 1.003:
                                             inf = get_klines1(symb_USDT, '1m', None, 5)
                                             min_price = 999
                                             max_price = 0
